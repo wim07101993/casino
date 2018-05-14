df8a1a566a62887acdacdd6c1e39bf6a8a1c2c77
wim van laer <van.laer.wim@gmail.com>
2018-05-14 17:06:48 +0200

    Added unity

--- SlotMachine/App.xaml.cs
+++ SlotMachine/App.xaml.cs
@@ -1,6 +1,24 @@
-﻿namespace SlotMachine
+﻿using System.Windows;
+using SlotMachine.Views;
+using Unity;
+
+namespace SlotMachine
 {
     public partial class App
     {
+        public IUnityContainer UnityContainer { get; private set; }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            UnityContainer = new UnityContainer();
+
+            MainWindow = UnityContainer.Resolve<MainWindow>();
+            if (MainWindow != null)
+                MainWindow.Show();
+            else
+                MessageBox.Show("No window to show.");
+        }
     }
 }
