1fb2e6b755513344dea9647bca8d2c6116b3c1a2
wim van laer <van.laer.wim@gmail.com>
2018-05-17 21:06:22 +0200

    added some properties to colortheme

--- SlotMachine/Models/ColorTheme.cs
+++ SlotMachine/Models/ColorTheme.cs
@@ -29,5 +29,35 @@ namespace SlotMachine.Models
             get => _accentColors;
             set => SetProperty(ref _accentColors, value);
         }
+
+        public Color Primary700
+            => PrimaryColors != null && PrimaryColors.ContainsKey("Primary700")
+                ? PrimaryColors["Primary700"]
+                : default(Color);
+
+        public Color Primary400
+            => PrimaryColors != null && PrimaryColors.ContainsKey("Primary400")
+                ? PrimaryColors["Primary400"]
+                : default(Color);
+
+        public Color Primary200
+            => PrimaryColors != null && PrimaryColors.ContainsKey("Primary200")
+                ? PrimaryColors["Primary200"]
+                : default(Color);
+
+        public Color Accent700
+            => AccentColors != null && AccentColors.ContainsKey("Accent700")
+                ? AccentColors["Accent700"]
+                : default(Color);
+
+        public Color Accent400
+            => AccentColors != null && AccentColors.ContainsKey("Accent400")
+                ? AccentColors["Accent400"]
+                : default(Color);
+
+        public Color Accent200
+            => AccentColors != null && AccentColors.ContainsKey("Accent200")
+                ? AccentColors["Accent200"]
+                : default(Color);
     }
 }
