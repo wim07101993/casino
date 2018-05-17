663efb59956725337822d89af6014b1d7adb3fa9
wim van laer <van.laer.wim@gmail.com>
2018-05-17 20:08:04 +0200

    changed checkboxes to icons + made width of drawer changable

---
+++ SlotMachine/Views/Controls/IconCheckBox.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace SlotMachine.Views.Controls
+{
+    public class IconCheckBox : CheckBox
+    {
+        public static readonly DependencyProperty IconProperty = DependencyProperty.Register(
+            nameof(Icon),
+            typeof(VisualBrush),
+            typeof(IconCheckBox),
+            new PropertyMetadata(default(VisualBrush)));
+
+
+        static IconCheckBox()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(IconCheckBox), new FrameworkPropertyMetadata(typeof(IconCheckBox)));
+        }
+
+
+        public VisualBrush Icon
+        {
+            get => (VisualBrush) GetValue(IconProperty);
+            set => SetValue(IconProperty, value);
+        }
+    }
+}
