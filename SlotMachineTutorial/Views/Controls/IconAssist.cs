fe01eafa1386faa54fffcec3bd3bd886762e3042
wim van laer <wim.van_laer@hotmail.com>
2019-03-05 19:08:15 +0100

    added icon to togglebutton

---
+++ SlotMachineTutorial/Views/Controls/IconAssist.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using System.Windows.Media;
+
+namespace SlotMachineTutorial.Views.Controls
+{
+    public static class IconAssist
+    {
+        public static readonly DependencyProperty IconProperty = DependencyProperty.RegisterAttached(
+            "Icon",
+            typeof(Visual), 
+            typeof(IconAssist),
+            new PropertyMetadata(default(Visual)));
+
+        public static void SetIcon(DependencyObject element, object value) => element.SetValue(IconProperty, value);
+
+        public static Visual GetIcon(DependencyObject element) => (Visual) element.GetValue(IconProperty);
+    }
+}
