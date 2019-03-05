ad7be00dd2d355e6659f31cb33b06092b13f1d4d
wim van laer <wim.van_laer@hotmail.com>
2019-03-05 18:13:03 +0100

    added animation for slidebutton

---
+++ SlotMachineTutorial/Views/Converters/MultiplyConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace SlotMachineTutorial.Views.Converters
+{
+    public class MultiplyConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            var result = 1.0;
+            foreach (var value in values)
+                if (value is double d)
+                    result *= d;
+
+            return result;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
