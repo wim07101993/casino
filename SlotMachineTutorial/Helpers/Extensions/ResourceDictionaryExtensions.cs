a64e1d3c9b10e83e11a50f751fc75198ffce27c5
wim van laer <van.laer.wim@gmail.com>
2019-03-05 13:17:03 +0100

    Added symbol provider

---
+++ SlotMachineTutorial/Helpers/Extensions/ResourceDictionaryExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Windows;
+
+namespace SlotMachineTutorial.Helpers.Extensions
+{
+    public static class ResourceDictionaryExtensions
+    {
+        public static Dictionary<string, T> ToDictionary<T>(this ResourceDictionary This)
+        {
+            var ret = new Dictionary<string, T>();
+
+            foreach (DictionaryEntry entry in This)
+                ret.Add(entry.Key.ToString(), (T) entry.Value);
+
+            return ret;
+        }
+    }
+}
---
+++ SlotMachineTutorial/Services/ISymbolProvider.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using SlotMachineTutorial.Models;
+
+namespace SlotMachineTutorial.Services
+{
+    public interface ISymbolProvider
+    {
+        List<SymbolTheme> SymbolThemes { get; }
+    }
+}
